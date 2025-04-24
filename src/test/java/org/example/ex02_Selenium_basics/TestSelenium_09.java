c2e7d7b3c2f6f3e0f64197417f6416cdc361441a
Yashaswini200 <yashusuresh2050@gmail.com>
2025-04-24 19:33:43 +0530

    API

---
+++ src/test/java/org/example/ex02_Selenium_basics/TestSelenium_09.java
@@ -0,0 +1,25 @@
+package org.example.ex02_Selenium_basics;
+
+import io.qameta.allure.Description;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+public class TestSelenium_09 {
+    @Description("open the url")
+    @Test
+    public void test_selenium_01() {
+        WebDriver driver = new EdgeDriver();
+        driver.get("https://google.com");
+        driver.navigate().to("https://bing.com");
+        driver.navigate().back();
+        driver.navigate().forward();
+        driver.navigate().refresh();
+        try {
+            Thread.sleep(5000); // Sleep for 5 seconds
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt(); // Restore interrupt status
+            System.out.println("Thread was interrupted: " + e.getMessage());
+        }        driver.quit();
+    }
+}
