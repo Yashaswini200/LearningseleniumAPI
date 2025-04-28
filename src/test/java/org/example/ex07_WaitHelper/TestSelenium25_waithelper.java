aeb85ac4eba41d598928857f5d932e4bb5bf9ee8
Yashaswini200 <yashusuresh2050@gmail.com>
2025-04-28 19:47:28 +0530

    API

---
+++ src/test/java/org/example/ex07_WaitHelper/TestSelenium25_waithelper.java
@@ -0,0 +1,8 @@
+package org.example.ex07_WaitHelper;
+
+public class TestSelenium25_waithelper {
+    public  static void wait_time(int time) throws Exception{
+        Thread.sleep(time);
+    }
+
+}
